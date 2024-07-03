8634bdb6760bba0be711aa72e1e6fcfa12c48aa5
Thomas Gerbet <thomas@gerbet.me>
2024-07-03 21:53:44 +0200

    fix: Do not use polyfill scripts from polyfill.io
    The polyfill.io domain is under the control of a bad actor,
    see https://sansec.io/research/polyfill-supply-chain-attack
    
    Removing it completely should be fine, it is no more needed
    by modern browsers now that IE11 is not a concern anymore.

--- lib/jekyll-spaceship/processors/mathjax-processor.rb
+++ lib/jekyll-spaceship/processors/mathjax-processor.rb
@@ -7,7 +7,6 @@ module Jekyll::Spaceship
     def self.config
       {
         'src' => [
-          'https://polyfill.io/v3/polyfill.min.js?features=es6',
           'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
         ],
         'config' => {
